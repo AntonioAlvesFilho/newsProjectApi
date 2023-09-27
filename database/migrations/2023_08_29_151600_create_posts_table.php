6078a545e8557ccdccb55c56a40384210c1c6444
root <root@ip-172-31-94-220.ec2.internal>
2023-09-27 20:30:53 +0000

    change-post-column

--- database/migrations/2023_08_29_151600_create_posts_table.php
+++ database/migrations/2023_08_29_151600_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
 						$table->id();
 						$table->string('image');
 						$table->string('title');
-						$table->string('description');
+						$table->longText('description');
 						$table->unsignedBigInteger('user_id');
 						$table->foreign('user_id')
 							->references('id')
